b3b960b3ccbd15e2ffd01f2b95299ae90eb901b4
bart <bielski.bart1012@gmail.com>
2024-12-06 10:19:14 +0000

    fix: Add plateau to mission control for tracking

--- MarsRover/MarsRover/MissionControl.cs
+++ MarsRover/MarsRover/MissionControl.cs
@@ -3,16 +3,28 @@
     public class MissionControl
     {
         public List<Position> RoverPositions { get; private set; }
+        public Plateau plateau { get; private set; }
 
-        public MissionControl()
+        public MissionControl(Plateau plateau)
         {
             RoverPositions = new();
+            this.plateau = plateau;
         }
 
-        public Rover InitaliseNewRover(Position position)
+        public Rover? InitaliseNewRover(Position position)
         {
+            if (position.X >= plateau.X / 2 || position.Y >= plateau.Y / 2)
+            {
+                Console.WriteLine("Invalid Input");
+                return null;
+            }
             RoverPositions.Add(position);
-            return new Rover(position);
+            return new Rover(position, this.plateau);
+        }
+
+        public void SetTerrain(Plateau plateau)
+        {
+            this.plateau = plateau;
         }
     }
 }
